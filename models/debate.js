b5ee522433fcec843bd506e96e484c0bf1eab3dd
Jon Berry <jonpber@gmail.com>
2017-08-18 12:06:35 -0700

    added sequelize model for debate

---
+++ models/debate.js
@@ -0,0 +1,34 @@
+module.exports = function(sequelize, Datatypes){
+	var Debate = sequelize.define("Debate", {
+		debate_topic: {
+			type: Datatypes.STRING,
+			allowNull: false,
+		},
+		sideA: {
+			type: Datatypes.STRING,
+			defaultValue: ""
+		},
+		votesA: {
+			type: Datatypes.INTEGER,
+			defaultValue: 0
+		},
+
+		sideB: {
+			type: Datatypes.STRING,
+			defaultValue: ""
+		},
+		votesB: {
+			type: Datatypes.INTEGER,
+			defaultValue: 0
+		},
+
+		chatLog: {
+			type: Datatypes.TEXT,
+			defaultValue: ""
+		}
+	});
+
+	return Debate;
+
+}
+
